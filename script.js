031317952fb9006f978dcea6bf8671e0773bb4cd
Cepsycal <85412800+Cepsycal@users.noreply.github.com>
2024-09-16 17:58:18 -0400

    fix: Make humanchoice return a number

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
+let humanscore = 0;
+let computerscore = 0;
+
 function getComputerChoice () {
     return Math.round(Math.random() * 2);
 }
 
 function getHumanChoice () {
     //Ask what they choose and turn it into human choice (only string)
-    let humanchoice = prompt("Choose either Rock, Paper, or Scissors")
-    console.log(humanchoice)
+    let humanChoice = prompt("Choose either Rock, Paper, or Scissors")
+    console.log(humanChoice)
     //Make human choice all lowercarse
-    humanchoice = (humanchoice.toLowerCase())
-    console.log(humanchoice)
+    humanChoice = (humanChoice.toLowerCase())
+    console.log(humanChoice)
     //Check if human choice is equal to the available choices. If not, tell human to try again
     //Turn human chocie into a number so we can compute it
-    switch (humanchoice) {
-        case humanchoice = "rock" : humanchoice = 0;
-        console.log(humanchoice);
+    switch (humanChoice) {
+        case humanChoice = "rock" : humanchoice = 0;
+        return 0;
         break;
-        case humanchoice = "paper" : humanchoice = 1;
-        console.log(humanchoice);
+        case humanChoice = "paper" : humanchoice = 1;
+        return 1;
         break;
-        case humanchoice = "scissors": humanchoice = 2;
-        console.log(humanchoice)
+        case humanChoice = "scissors": humanchoice = 2;
+        return 2;
         break;
     }
     }
