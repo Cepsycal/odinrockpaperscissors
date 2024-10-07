12943913c7140d7c4d3fee0f5dfbae50e465a7e7
Cepsycal <85412800+Cepsycal@users.noreply.github.com>
2024-10-06 23:28:33 -0400

    feat: Create one round of the game
     The game can play one round, and it tracks the
     score using a variable.

--- script.js
+++ script.js
@@ -1,44 +1,69 @@
-let humanscore = 0;
-let computerscore = 0;
-let computerChoice = 0;
-let humanNum = 0;
+let computerChoice = 5;
+let humanChoice = 10;
 
 function getComputerChoice () {
-    let computerNum = Math.round(Math.random() * 2);
+    computerNum = Math.round(Math.random() * 2.49);
+    console.log(computerNum);
     switch (computerNum) {
         case computerNum = 0 :
-        return "rock";
+        computerChoice = "rock";
+        return 0;
         case computerNum = 1 :
-        return "paper";
+        computerChoice = "paper";
+        return 1;
         case computerNum = 2 :
-        return "scissors";
+        computerChoice = "scissors";
+        return 2;
     }
 }
 
 function getHumanChoice () {
     //Ask what they choose and turn it into human choice (only string)
     let humanChoice = prompt("Choose either Rock, Paper, or Scissors")
-    console.log(humanChoice)
     //Make human choice all lowercarse
     humanChoice = (humanChoice.toLowerCase())
-    console.log(humanChoice)
-    //Check if human choice is equal to the available choices. If not, tell human to try again
+    console.log(humanChoice);
     //Turn human chocie into a number so we can compute it
     switch (humanChoice) {
-        case humanChoice = "rock": 
-        return "rock";
+        case humanChoice = "rock":
+        return 0;
         case humanChoice = "paper":
-        return "paper";
+        return 1;
         case humanChoice = "scissors":
-        return "scissors";
-        default: alert("Please Enter Choice Again")
+        return 2;
+        //Check if human choice is equal to the available choices. If not, tell human to try again
+        default: 
+        alert("Please Enter Choice Again");
     }
-    }
-
-function playRound (humanSelection, computerSelection)  {
-    console.log(humanSelection);
-    console.log(computerSelection);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playRound (humanSelection, computerSelection)  {
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    let humanScore = 0;
+    let computerScore = 0;
+    //Calculate who won based on the number values given
+    if (humanSelection - computerSelection == 0) {
+        //play again no add point
+        alert("It is a tie!");
+    } else if (humanSelection - computerSelection == 1 || 
+                humanSelection - computerSelection == -2) {
+        humanScore = ++humanScore;
+        alert("You won this round! Your score is " + humanScore + " now!");
+        console.log(humanScore);
+    } else if (humanSelection - computerSelection == -1 || 
+                humanSelection - computerSelection == 2) {
+        computerScore = ++computerScore;
+        alert("The computer won this round! The computer's score is " + computerScore + " now!");
+        console.log("computer");
+        console.log(computerScore);
+    } else {
+        console.log(humanSelection - computerSelection);
+        console.log(humanSelection);
+        console.log(computerSelection);
+        console.log("The calculations aren't right");
+    }   
+}
+
+
+
